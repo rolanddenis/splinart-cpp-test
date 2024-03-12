de786d300bb2199205686c831105621e85594dc4
Roland DENIS <denis@math.univ-lyon1.fr>
2024-03-12 17:11:41 +0100

    fix: Fixing errors in example

--- examples/circle.cpp
+++ examples/circle.cpp
@@ -9,14 +9,15 @@
 
 #include <splinart/build_img.hpp>
 #include <splinart/imshow.hpp>
-usingnamespacext::placeholders;
+using namespace xt::placeholders;
 
-int main(intargc, char* argv[])
+int main(int argc, char* argv[])
 {
-    std::size_timg_size   = 1000;
-    std::size_tnb_samples = 10000;
-    splinart::Circlecircle({0.5, 0.5}, 0.3, 75);
-    autoimg = splinart::build_img({img_size, img_size}, {circle}, nb_samples);
+    std::size_t img_size   = 1000;
+    std::size_t nb_samples = 10000;
+    splinart::Circle circle({0.5, 0.5}, 0.3, 75);
+    std::vector circles = {circle};
+    auto img            = splinart::build_img({img_size, img_size}, circles, nb_samples);
     splinart::imshow(img);
-    return0;
+    return 0;
 }
